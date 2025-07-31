511a5dcb0625c46cdddbaf2d2effff3bf9867703
marketingtoolslat <marketingtoolslat@gmail.com>
2025-07-31 17:02:11 -0400

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-echo "Instalando navegador headless con Playwright..."
-python -m playwright install chromium
-echo "Ejecutando interfaz Gradio con ScraperGPT Pro++..."
 python3 dashboard_gradio.py
